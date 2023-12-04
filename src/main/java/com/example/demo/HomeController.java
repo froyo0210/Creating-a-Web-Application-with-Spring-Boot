6ba2390861f212cf2492ecec3a2baf8ff79b7376
Momo <momobelia0210@gmail.com>
2023-12-05 03:33:59 +0900

    Creating a Web Application with Spring Boot

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -3,11 +3,15 @@ package com.example.demo;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
 @Controller
 public class HomeController {
+
+    // use final here cuz we want it to storage some immutable
     private final VideoService videoService;
 
     public HomeController(VideoService videoService){
@@ -15,8 +19,16 @@ public class HomeController {
     }
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model) {
         model.addAttribute("videos", videoService.getVideos());
         return "index";
+    }
+
+    // why return a String?
+    // what ModelAttribute do ? get form input
+    @PostMapping("/new-video")
+    public String newVideo(@ModelAttribute Video newVideo) {
+        videoService.create(newVideo);
+        return "redirect:/";
     }
 }
--- src/main/java/com/example/demo/VideoService.java
+++ src/main/java/com/example/demo/VideoService.java
@@ -2,6 +2,7 @@ package com.example.demo;
 
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -11,7 +12,15 @@ public class VideoService {
             new Video("Don't do THIS to your own CODE!"),
             new Video("SECRETS to fix BROKEN CODE!"));
 
-    public List<Video> getVideos(){
+    public List<Video> getVideos( ){
         return videos;
+    }
+
+    public Video create(Video newVideo) {
+        List<Video> extend = new ArrayList<>(videos);
+        extend.add(newVideo);
+        this.videos = List.copyOf(extend);
+        return newVideo;
+
     }
 }
